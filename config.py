0b88fc9483b651c146ad6b68f94717024c685738
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2021-01-02 22:05:21 +0530

    structure created

---
+++ config.py
@@ -0,0 +1,6 @@
+from os import environ
+HOME="http://localhost/services"
+SERVICES="http://localhost"
+ADDITION="http://localhost:91/addition"
+SUBTRACTION="http://localhost:92/subtraction"
+MULTIPLICATION="http://localhost:93/multiplication"
