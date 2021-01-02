e646362e5b623490d1ae5235fb009402a2549f9d
Shriram Tiwari <shriram.tiwari@pixelque.com>
2021-01-02 22:05:21 +0530

    structure created

---
+++ commit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+date=2
+while [ $date -le 30 ]
+do
+	powershell set-date -date $date-12-2020
+	for i in {0..100}
+	do
+		echo hello >>  f.txt
+		git commit f.txt -m "any comment"
+	done
+	((date=date+1))
+done
