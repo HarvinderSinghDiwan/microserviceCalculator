e6f8d5be77bbd64faabf87dc58e3fb7dd743d119
Harvinder Singh khalsa <harvindersinghdiwan@gmail.com>
2022-03-06 02:06:04 +0530

    fixed url issued and actions

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from os import environ
-SERVICES="http://0.0.0.0/services"
-HOME="http://0.0.0.0"
+FRONTEND=environ.get("FRONTEND")
 ADDITION=environ.get("ADDITION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:91/addition"
 SUBTRACTION=environ.get("SUBTRACION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:92/subtraction"
 MULTIPLICATION=environ.get("MULTIPLICATION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:93/multiplication"
--- main.py
+++ main.py
@@ -24,14 +24,14 @@ def genAuth():
     data = res.read()
     bearer_token=data.decode("utf-8").split(",")
     session['name']=bearer_token
-    return str(open("index_style.css","r").read())+str(open("index.html","r").read()).format(str(bearer_token[0].split(":")[1]),app.config["SERVICES"])
+    return str(open("index_style.css","r").read())+str(open("index.html","r").read()).format(str(bearer_token[0].split(":")[1]),app.config["FRONTEND"])
 
 @app.route("/services")
 def services():
     #getr=requests.get("http://localhost:5000/gif")
      #send_file(getr.text,mimetype='image/gif')
     if session:
-        return str(open("main_style.css","r").read())+str(open("main.html","r").read()).format(app.config['HOME'],app.config['HOME'],app.config['HOME'])
+        return str(open("main_style.css","r").read())+str(open("main.html","r").read()).format(app.config['FRONTEND'],app.config['FRONTEND'],app.config['FRONTEND'])
     return genAuth()
 @app.route("/addition")
 def add():
@@ -39,7 +39,7 @@ def add():
     x=int(args.get('fnum'))
     y=int(args.get('lnum'))
     if session:
-        result=requests.get("{}?x={}&y={}".format(app.config['ADDITION'],x,y))
+        result=requests.get("http://{}addition?x={}&y={}".format(app.config['ADDITION'],x,y))
         
         return str(open("style.css","r").read())+str(open("result.html","r").read()).format("""Your request has been processed 
                                                                                             successfully and the SUM is given below""",result.text)
@@ -50,7 +50,7 @@ def subtract():
     x=int(args.get('fnum'))
     y=int(args.get('lnum'))
     if session:
-        result=requests.get("{}?x={}&y={}".format(app.config['SUBTRACTION'],x,y))
+        result=requests.get("http://{}/subtraction?x={}&y={}".format(app.config['SUBTRACTION'],x,y))
         return str(open("style.css","r").read())+str(open("result.html","r").read()).format("""Your request has been processed 
                                                                                             successfully and the DIFFERENCE is given below""",result.text)
     return genAuth()
@@ -60,7 +60,7 @@ def multiply():
     x=int(args.get('fnum'))
     y=int(args.get('lnum'))
     if session:
-        result=requests.get("{}?x={}&y={}".format(app.config['MULTIPLICATION'],x,y))
+        result=requests.get("http://{}multiplication?x={}&y={}".format(app.config['MULTIPLICATION'],x,y))
         return str(open("style.css","r").read())+str(open("result.html","r").read()).format("""Your request has been processed 
                                                                                             successfully and the PRODUCT is given below""",result.text)
     return genAuth()
