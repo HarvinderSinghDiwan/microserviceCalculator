454dbf547005ea31b29c16c0fe74078f746dfb2d
Harvinder Singh khalsa <harvindersinghdiwan@gmail.com>
2022-03-06 02:36:47 +0530

    changed spelling of subtraction

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from os import environ
 FRONTEND=environ.get("FRONTEND")
 ADDITION=environ.get("ADDITION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:91/addition"
-SUBTRACTION=environ.get("SUBTRACION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:92/subtraction"
+SUBTRACTION=environ.get("SUBTRACTION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:92/subtraction"
 MULTIPLICATION=environ.get("MULTIPLICATION")#"http://ec2-65-1-106-225.ap-south-1.compute.amazonaws.com:93/multiplication"
